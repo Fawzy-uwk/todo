f22f6a4b0fc7289a2c92c26d5655e726c09f5def
Fawzy Mohamed <75136326+Fawzy-uwk@users.noreply.github.com>
2025-03-14 02:49:11 +0200

    done

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App.jsx";
 import { store } from "./features/store.jsx";
 import { Provider } from "react-redux";
-import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
